2463c44c8a64120e02c5e5c775a667ca2b23c6b3
ricardog <ricardog@localhost>
2016-03-30 14:18:14 -0400

    second commit

--- src/edu/saintjoe/cs/ricardog/SimpleDotComGamerhg/SimpleDotComGame.java
+++ src/edu/saintjoe/cs/ricardog/SimpleDotComGamerhg/SimpleDotComGame.java
@@ -8,15 +8,9 @@ public class SimpleDotComGame {
 		
 		int[] locations = {2,3,4};
 		dot.setLocationCells(locations);
-		
 		String userGuess = "2";
 		String result = dot.checkYourself(userGuess);
-		String testResult = "failed";
-		if (result.equals("hit") ) {
-			testResult = "passed";
-		}
 		
-		System.out.println(testResult);
 
 
 		
